f22ef04ec6ce0a302c514240ef698cf1e7c11b5a
hrookim <hrookim@gmail.com>
2023-02-07 00:20:38 +0900

    [feat] add loaders

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,24 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: {
-    main: './src/app.js'
+    main: './src/app.js',
   },
   output: {
     filename: '[name].js',
     path: path.resolve('./dist'),
-  }
-}
+  },
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        loader: 'url-loader',
+        options: {
+          publicPath: './dist/',
+          name: '[name].[ext]?[hash]',
+          limit: 10000,
+        },
+      },
+    ],
+  },
+};
